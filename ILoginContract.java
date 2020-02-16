f990c173e735f96f4006ac4b28691d512f60459e
JianLiangLiang-art <60775136+JianLiangLiang-art@users.noreply.github.com>
2020-02-16 13:56:18 +0800

    Add files via upload

---
+++ ILoginContract.java
@@ -0,0 +1,59 @@
+package com.wd.health.contract;
+
+/**
+ * @Description: 类作用描述
+ * @Author: 高晨凯
+ * @CreateDate: 2020/1/7 14:02
+ */
+
+import com.wd.health.base.IBaseView;
+import com.wd.health.model.bean.LoginBean;
+
+import java.util.Map;
+
+/**
+ * 契约类，方便统一管理相关接口
+ * <p>
+ * <p>
+ * 声明规范：
+ * 1、命名规范为 "I${业务模块}Contract" 格式，如：登录模块 Contract 命名为 "ILoginContract"
+ * 2、内部接口命名规范为 IView 、IPresenter 、IModel、IModelCallback   ps：内部接口命名不加业务名字
+ */
+public interface ILoginContract {
+
+    /**
+     * view层  命名必须是IView
+     */
+    interface IView extends IBaseView {
+        void onLoginSuccess(LoginBean loginBean);
+
+        void onLoginFailure(Throwable e);
+    }
+
+    /**
+     * presenter层   命名必须是IPresenter
+     */
+    interface IPresenter {
+        void login(Map<String, Object> paramsMap);
+    }
+
+    /**
+     * model层   命名必须是IModel
+     */
+    interface IModel {
+        void login(Map<String, Object> paramsMap, IModelCallback callback);
+
+        /**
+         * model层中的接口回调
+         */
+        interface IModelCallback {
+            void onLoginSuccess(LoginBean loginBean);
+
+            void onLoginFailure(Throwable e);
+        }
+    }
+
+
+
+
+}
